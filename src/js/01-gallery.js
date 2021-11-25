60dc215a8bea4e0e5f46747680cb379b39845601
Oleksandr Arseniuk <korol1511@gmail.com>
2021-11-25 18:51:34 +0200

    HW 8 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Change code below this line
+
+const galleryFind = document.querySelector(".gallery");
+
+function createImagesAll(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<a
+  class="gallery__link"
+  href='${original}'>
+  <img
+    class="gallery__image"
+    src='${preview}'
+    data-source='${original}'
+    alt='${description}'
+  />
+</a>`;
+    })
+    .join('');
+}
+
+const imageViewAll = createImagesAll(galleryItems);
+galleryFind.insertAdjacentHTML('beforeend', imageViewAll);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
 // Change code below this line
 
 console.log(galleryItems);
